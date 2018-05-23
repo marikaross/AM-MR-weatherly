2b257cfb376011c6ae520a85eae8bcd1187cb76c
Marika Ross <marika.ross@gmail.com>
2018-05-23 05:36:30 -0600

    Render Hourly Card

---
+++ src/Hourly-Card.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class HourlyCard extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return(
+      <div className='hourlyCard'>
+        <h2>{this.props.hour}</h2>
+        <h3><img src={this.props.condition}/></h3>
+        <h3>{this.props.projectedTemp}</h3>
+      </div>
+      )
+  }
+}
+
+export default HourlyCard; 
