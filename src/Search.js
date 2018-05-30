1fbf0871912c2122a6abb364ab22f3f877b51ed1
Marika Ross <marika.ross@gmail.com>
2018-05-30 08:52:19 -0600

    Pass props to search function

---
+++ src/Search.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+export default class Search extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      userInput: ''
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <input 
+          type="text" 
+          placeholder="Example search" 
+          value={this.state.userInput}
+          onChange={(event) => {
+          this.setState({
+          userInput: event.target.value
+       
+        })}} />
+        <button onClick={(event) => 
+          {this.props.fetchWeather(this.state.userInput)}}
+        >Submit</button> 
+      </div>
+      )
+  }
+}
+
+
